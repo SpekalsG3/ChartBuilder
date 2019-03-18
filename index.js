44689848e29145a9f0554dcd5d0646fc0da5d04b
SpekalsG3 <karpick.kirill@gmail.com>
2019-03-19 00:10:04 +0300

    Made resized charts look better. Adapted app to work with more than 2 charts on one graph. Added buttons to toggle charts. Added window to show info on a day mouse hovered

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-var joined = document.getElementById("joined"),
-	left = document.getElementById("left"),
-	j_ctx = joined.getContext("2d"),
-	l_ctx = left.getContext("2d");
-
-var joined_mini = document.getElementById("joined_mini"),
-	left_mini = document.getElementById("left_mini"),
-	j_ctx_mini = joined_mini.getContext("2d"),
-	l_ctx_mini = left_mini.getContext("2d");
-
 var scrollArea = document.getElementById("scrollArea"),
 	pointer = document.getElementById("point"),
 	borderLeft = document.getElementById("left_border"),
@@ -16,11 +6,32 @@ var scrollArea = document.getElementById("scrollArea"),
 var dataAnchores = document.getElementById("data").children,
 	timeAnchores = document.getElementById("timeStamps");
 
+var switchJoined = document.getElementById("switchJoined"),
+	switchLeft = document.getElementById("switchLeft");
+
+var sumJoined = document.getElementById("sumJoined"),
+	sumLeft = document.getElementById("sumLeft");
+
+var mainCharts = document.getElementById("main"),
+	miniCharts = document.getElementById("scroll");
+
+var buttons = document.getElementById("buttons"),
+	hintBox = document.getElementById("hints");
+
+var charts = [],
+	charts_mini = [],
+	ctx_main = [],
+	ctx_mini = [],
+	switchers = [],
+	showFlag = [],
+	hints = [];
+
 var min,
 	data, xhr,
 	max,
 	step, step_mini,
-	width;
+	cnvsWidth,
+	cnvsHeight = 400;
 
 if (window.innerWidth > 600) {
 	min = 600;
@@ -44,14 +55,15 @@ xhr.open("GET", "chart_data.json");
 xhr.responseType = "json";
 xhr.send();
 
-function InitCanvasProperties(cnvs, ctxt, leftGap, color, lineWidth) {
+function InitCanvasProperties(cnvs, ctxt, leftGap, start, color, lineWidth) {
 	cnvs.style.left = leftGap;
-	cnvs.width = width;
-	cnvs.style.width = width + "px";
+	//cnvs.height = cnvsHeight;
+	cnvs.width = cnvsWidth;
+	cnvs.style.width = cnvsWidth + "px";
 	ctxt.strokeStyle = color;
 	ctxt.lineWidth = lineWidth;
 	ctxt.beginPath();
-	ctxt.moveTo(0, cnvs.height -(data[0].columns[1][0] * cnvs.height)/(max*1.1));
+	ctxt.moveTo(0, cnvs.height - (start * cnvs.height) / (max*1.1));
 }
 
 function DrawBigChart(cnvs, ctxt, clmn, iter) {
@@ -68,64 +80,144 @@ setTimeout(function(){
 
 	for (var i = 0; i < data[0].columns.length; i++) {
 		data[0].columns[i].shift();
+
+		if (i > 0) {
+			var bigCanvas = document.createElement("canvas");
+			bigCanvas.setAttribute("height", 400);
+			bigCanvas.setAttribute("width", 24);
+			//bigCanvas.style.top = -(bigCanvas.height * (i-1)) + "px";
+			mainCharts.appendChild(bigCanvas);
+			charts[i-1] = bigCanvas;
+			ctx_main[i-1] = bigCanvas.getContext("2d");
+
+			var smallCanvas = document.createElement("canvas");
+			smallCanvas.setAttribute("height", 80);
+			//smallCanvas.style.top = -(smallCanvas.height * (i-1)) + "px";
+			miniCharts.appendChild(smallCanvas);
+			charts_mini[i-1] = smallCanvas;
+			ctx_mini[i-1] = smallCanvas.getContext("2d");
+
+			var btn = document.createElement("div");
+			btn.className = "switchers";
+			var swtch = document.createElement("div");
+			swtch.classList.add("swtchbtn", "flex");
+			var led = document.createElement("div");
+			led.classList.add("led", "flex");
+			led.style.background = data[0].colors["y"+(i-1)];
+			var name = document.createElement("span");
+			name.innerHTML = data[0].names["y"+(i-1)];
+			led.appendChild(document.createElement("div"));
+			swtch.appendChild(led);
+			swtch.appendChild(name);
+			btn.appendChild(swtch);
+			btn.setAttribute("index", i-1);
+			switchers[i-1] = btn;
+			showFlag[i-1] = true;
+			buttons.appendChild(btn);
+
+			var hint = document.createElement("div");
+			hint.className = "sumContainer";
+			var text = document.createElement("div");
+			text.className = "flex";
+			text.style.color = data[0].colors["y"+(i-1)];
+			text.style.flexDirection = "column";
+			var num = document.createElement("span");
+			num.style.fontSize = "20px";
+			num.innerHTML = 1111;
+			text.appendChild(num);
+			text.innerHTML += data[0].names["y"+(i-1)];
+			hint.appendChild(text);
+			hintBox.appendChild(hint);
+			hints[i-1] = hint;
+		}
 	}
 
-	max = Math.max( Math.max.apply(null, data[0].columns[1]),
-					Math.max.apply(null, data[0].columns[2]));
+	maxJoined = Math.max.apply(null, data[0].columns[1]);
+	maxLeft = Math.max.apply(null, data[0].columns[2]);
+	max = Math.max(maxJoined, maxLeft);
 
 	for (var i = 0; i < dataAnchores.length; i++) {
 		dataAnchores[i].innerHTML = Math.floor((max / (dataAnchores.length-1)) * (i));
 	}
 
-	step = joined.width;
-	width = (data[0].columns[1].length-1) * step;
-	step_mini = (step * min) / width;
+	step = charts[0].width * 2.5;
+	cnvsWidth = (data[0].columns[1].length-1) * step;
+	step_mini = (step * min) / cnvsWidth;
 
-	pointer.style.display = "block";
-	pointer.style.width = (min * min) / width + "px";
-	pointer.style.left = min - parseInt(pointer.style.width); + "px";
-
-
-	InitCanvasProperties(joined, j_ctx, -width + min + "px", data[0].colors.y0, 4);
-	InitCanvasProperties(joined_mini, j_ctx_mini, 0, data[0].colors.y0, 2);
-
-	InitCanvasProperties(left, l_ctx, -width + min + "px", data[0].colors.y1, 4);
-	InitCanvasProperties(left_mini, l_ctx_mini, 0, data[0].colors.y1, 2);
-
-
-	for (var i = 1; i < data[0].columns[0].length+1; i++) {
-
-		var timeStamp = document.createElement("div");
-		var time = new Date(data[0].columns[0][i-1]);
-		timeStamp.className = "timeStamp";
-		timeStamp.innerHTML = time.toDateString().substr(4,6);
-		timeAnchores.appendChild(timeStamp);
-
-		DrawBigChart(joined, j_ctx, 1, i);
-		DrawMiniChart(joined_mini, j_ctx_mini, 1, i);
-
-		DrawBigChart(left, l_ctx, 2, i);
-		DrawMiniChart(left_mini, l_ctx_mini, 2, i);
-
+	for (var j = 0; j < charts.length; j++) {
+		InitCanvasProperties(charts[j], ctx_main[j], -cnvsWidth + min + "px", data[0].columns[j+1][0], data[0].colors["y"+j], 6);
+		InitCanvasProperties(charts_mini[j], ctx_mini[j], 0, data[0].columns[j+1][0], data[0].colors["y"+j], 2);
 	}
 
-	j_ctx.stroke();
-	j_ctx_mini.stroke();
+	for (var j = 0; j < charts.length; j++) {
+		for (var i = 1; i < data[0].columns[0].length+1; i++) {
 
-	l_ctx.stroke();
-	l_ctx_mini.stroke();
+			/*var time = new Date(data[0].columns[0][i-1]);
+			var timeStamp = document.createElement("div");
+			timeStamp.innerHTML = time.toDateString().substr(4,6);
+
+			timeAnchores.appendChild(timeStamp);*/
+
+			DrawBigChart(charts[j], ctx_main[j], j+1, i);
+			DrawMiniChart(charts_mini[j], ctx_mini[j], j+1, i);
+
+		}
+	}
+
+	timeAnchores = timeAnchores.children;
+
+	for (var i = 0; i < ctx_main.length; i++) {
+		ctx_main[i].stroke();
+		ctx_mini[i].stroke();
+	}
+
+	cnvsWidth /= 2.5;
+
+	pointer.style.display = "block";
+	pointer.style.width = (min * min) / cnvsWidth + "px";
+	pointer.style.left = min - parseInt(pointer.style.width); + "px";
+
+	ChangeGraphRatio();
+
+
+	for (var i = 0; i < switchers.length; i++) {
+		switchers[i].onclick = function(event) {
+			event.preventDefault();
+			var index = parseInt(this.getAttribute("index"));
+
+			if (showFlag[index]) {
+				HideChart(this, charts[index]);
+				HideChart(this, charts_mini[index]);
+				//ResizeChart(joinedShow, left);
+				showFlag[index] = false;
+			} else {
+				ShowChart(this, charts[index]);
+				ShowChart(this, charts_mini[index]);
+				//ResizeChart(joinedShow, left);
+				showFlag[index] = true;
+			}
+		}
+	}
 }, 500);
 
 
 var scroll = false,
-	moveFree = false,
-	sizeFree = false,
+	moveFree = true,
+	sizeFree = true,
 	leftStart,
 	x_start;
 
 var widthStart,
 	leftGrabber = false,
 	rightGrabber = false;
+
+
+function MoveGraph() {
+	var move = -(parseInt(pointer.style.left) * cnvsWidth) / min + "px";
+	for (var i = 0; i < charts.length; i++) {
+		charts[i].style.left = move;
+	}
+}
 
 function CheckPointerOut() {
 	if (parseInt(pointer.style.left) > min - parseInt(pointer.style.width)) {
@@ -137,6 +229,7 @@ function CheckPointerOut() {
 	} else {
 		moveFree = true;
 	}
+	
 	MoveGraph();
 }
 
@@ -152,21 +245,14 @@ pointer.onmousedown = function(event) {
 	}
 }
 
-function MoveGraph() {
-	joined.style.left = -(parseInt(pointer.style.left) * width) / min + "px";
-
-	left.style.left = -(parseInt(pointer.style.left) * width) / min + "px";
-}
-
 scrollArea.onmousemove = function(event) {
 
 	if (scroll) {
 
-		CheckPointerOut()
+		CheckPointerOut();
 
 		if (moveFree) {
 			pointer.style.left = leftStart - x_start + event.clientX + "px";
-			MoveGraph();
 		}
 
 		CheckPointerOut();
@@ -181,6 +267,7 @@ borderLeft.onmousedown = function(event) {
 	x_start = event.clientX;
 	leftStart = parseInt(pointer.style.left);
 	scrollArea.style.display = "block";
+
 }
 borderRight.onmousedown = function(event) {
 	event.preventDefault();
@@ -190,15 +277,15 @@ borderRight.onmousedown = function(event) {
 	x_start = event.clientX;
 	leftStart = parseInt(pointer.style.left);
 	scrollArea.style.display = "block";
+
 }
 
 function ChangeGraphRatio() {
-	width = (min * min) / parseInt(pointer.style.width);
-	joined.style.width = width + "px";
-	joined.style.height = "400px";
-
-	left.style.width = width + "px";
-	left.style.height = "400px";
+	cnvsWidth = (min * min) / parseInt(pointer.style.width);
+	for (var i = 0; i < charts.length; i++) {
+		charts[i].style.width = cnvsWidth + "px";
+		charts[i].style.height = cnvsHeight + "px";
+	}
 
 	MoveGraph();
 }
@@ -222,41 +309,32 @@ function CheckPointerSize() {
 
 scrollArea.addEventListener("mousemove", function(event) {
 
-	if (leftGrabber) {
+	CheckPointerSize();
 
-		CheckPointerSize();
+	if (leftGrabber) {
 		
 		if (sizeFree) {
 			pointer.style.width = widthStart + x_start - event.clientX + "px";
 			pointer.style.left = leftStart - x_start + event.clientX + "px";
 		}
 
-		CheckPointerSize();
-
-	}
-});
-
-scrollArea.addEventListener("mousemove", function(event) {
-
-	if (rightGrabber) {
-
-		CheckPointerSize();
+	} else if (rightGrabber) {
 
 		if (sizeFree) {
 			pointer.style.width = widthStart - x_start + event.clientX + "px";
 		}
 
-		CheckPointerSize();
-
 	}
+	
+	CheckPointerSize();
 });
 
 scrollArea.onmouseout = function() {
 	scroll = false;
 	leftGrabber = false;
 	rightGrabber = false;
-	moveFree = false;
-	sizeFree = false;
+	moveFree = true;
+	sizeFree = true;
 	scrollArea.style.display = "none";
 }
 
@@ -264,13 +342,55 @@ scrollArea.onmouseup = function() {
 	scroll = false;
 	leftGrabber = false;
 	rightGrabber = false;
-	moveFree = false;
-	sizeFree = false;
+	moveFree = true;
+	sizeFree = true;
 	scrollArea.style.display = "none";
 }
 
+function ResizeChart(flag, chart) {
+	var lastHeight = cnvsHeight;
 
+	if (flag) {
+		max = Math.max(maxJoined, maxLeft);
+		cnvsHeight = max * Math.min(maxJoined, maxLeft) / parseInt(chart.style.height);
+		chart.style.top = max * 1.1 - cnvsHeight + "px";
+		console.log("hide");
+	} else {
+		max = Math.min(maxJoined, maxLeft);
+		cnvsHeight = max * Math.max(maxJoined, maxLeft) / parseInt(chart.style.height);
+		chart.style.top = -chart.height + "px";
+		console.log("show");
+	}
 
+	chart.style.height = cnvsHeight + "px";
+}
+
+function ShowChart(swticher, chart) {
+	swticher.children[0].children[0].children[0].style.width = 0;
+	swticher.children[0].children[0].children[0].style.height = 0;
+	chart.style.opacity = 1;
+}
+function HideChart(swticher, chart) {
+	swticher.children[0].children[0].children[0].style.width = "23px";
+	swticher.children[0].children[0].children[0].style.height = "23px";
+	chart.style.opacity = 0;
+}
+
+/*function ResizeLeftChart() {
+	var lastHeight = cnvsHeight;
+
+	if (joinedShow) {
+		max = Math.max(maxJoined, maxLeft);
+		cnvsHeight = max * Math.min(maxJoined, maxLeft) / parseInt(left.style.height);
+		left.style.top = -(joined.height - lastHeight + cnvsHeight) + "px";
+	} else {
+		max = Math.min(maxJoined, maxLeft);
+		cnvsHeight = max * Math.max(maxJoined, maxLeft) / parseInt(left.style.height);
+		left.style.top = -joined.height + "px";
+	}
+	
+	left.style.height = cnvsHeight + "px";
+}*/
 
 
 
@@ -335,7 +455,7 @@ setInterval(function() {
 
 	if (keys[3]) {
 		console.log("more");
-		if (true) { //parseInt(joined.style.width) + 24 < width) {
+		if (true) { //parseInt(joined.style.width) + 24 < cnvsWidth) {
 			joined.style.width = parseInt(joined.style.width) + 24 + "px";
 			joined.style.height = "400px";
 		} else {
